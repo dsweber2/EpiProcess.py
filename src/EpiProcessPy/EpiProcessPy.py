eaf0ca5e499b3628456f5c56c9a57fc3f0948d55
dsweber2 <david.weber2@pm.me>
2024-09-28 16:09:55 -0500

    init as_epi_snap

--- src/EpiProcessPy/EpiProcessPy.py
+++ src/EpiProcessPy/EpiProcessPy.py
@@ -13,6 +13,27 @@ class EpiSnapAccessor:
         if not set(obj.columns) >= {"geo_value", "time_value"}:
             raise AttributeError("Must have 'geo_value' and 'time_value'.")
 
+    def as_epi_snap(
+            self,
+            as_of=None,
+            extra_keys = []
+    ):
+        obj = self._obj
+        if as_of is None:
+            as_of = self._obj.time_value.max()
+        elif not isinstance(as_of, pd.Timestamp):
+            pass
+        key_names = ["geo_value", "time_value", *extra_keys]
+        if self._obj.index.names != key_names:
+            if obj.index.names == [None]:
+                drop_index = True
+            else:
+                drop_index = False
+            obj = obj.reset_index(drop = drop_index).set_index(key_names)
+        obj.attrs["as_of"] = as_of
+        return obj
+
+
     def slide(self):
         ...
 
